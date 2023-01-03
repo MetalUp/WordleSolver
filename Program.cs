3f6b344ca81769a54c93d68b4367a0dfb74900e0
Richard Pawson <rpawson@nakedobjects.net>
2023-01-03 10:21:50 +0000

    Turned back into one project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+var possible = AllPossibleAnswers;
+var outcome = "";
+while (outcome != "*****")
+{
+    var attempt = BestAttempt(possible, ValidWords);
+    Console.WriteLine(attempt);
+    outcome = Console.ReadLine();
+    possible = PossibleAnswersAfterAttempt(possible, attempt, outcome);
+}
+
+//Alternative program to analyse performance, tested over all 2,309 possible answer words
+//var dict = new Dictionary<int, int>();
+//foreach (var target in AllPossibleAnswers)
+//{
+//    var possible = AllPossibleAnswers;
+//    var outcome = "";
+//    var attempt = "RAISE";
+//    int count = 0;
+//    while (outcome != "*****")
+//    {
+//        Console.Write($"{attempt} ");
+//        count++;
+//        outcome = MarkAttempt(attempt, target);
+//        possible = PossibleAnswersAfterAttempt(possible, attempt, outcome);
+//        attempt = BestAttempt1(possible, ValidWords);
+//    }
+//    Console.WriteLine();
+//    if (dict.Keys.Contains(count))
+//    {
+//        dict[count]++;
+//    }
+//    else
+//    {
+//        dict[count] = 1;
+//    }
+//}
+//foreach (var attempts in dict.Keys.OrderBy(k => k))
+//{
+//    Console.WriteLine($"{attempts} attempts: {dict[attempts]} games");
+//}
--- Usings.cs
+++ Usings.cs
@@ -1,3 +1,4 @@
 global using NUnit.Framework;
 global using System.Collections.Immutable;
 global using static WordleFunctions;
+global using static DataDefinitions;
